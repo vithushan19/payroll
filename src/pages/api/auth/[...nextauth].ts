6eb44d679c3564da5bec60747f24d1dc03590169
Vithushan Namasivayasivam <vithushan19@gmail.com>
2023-01-15 12:58:05 -0500

    disable creating a user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,40 +7,40 @@ import { User } from "../users/getUser.js";
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    async signIn({ user }) {
-      const BASE_URL = process.env.NEXTAUTH_URL ?? "";
-      console.log("email", user.email);
+    signIn({ user }) {
+      // const BASE_URL = process.env.NEXTAUTH_URL ?? "";
+      // console.log("email", user.email);
 
-      await fetch(`${BASE_URL}/api/users/getUser`, {
-        method: "POST",
-        body: JSON.stringify({ email: user.email }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      })
-        .then((res) => res.json())
-        .then(async (res: User[]) => {
-          console.log("res", res);
+      // await fetch(`${BASE_URL}/api/users/getUser`, {
+      //   method: "POST",
+      //   body: JSON.stringify({ email: user.email }),
+      //   headers: {
+      //     "Content-type": "application/json; charset=UTF-8",
+      //   },
+      // })
+      //   .then((res) => res.json())
+      //   .then(async (res: User[]) => {
+      //     console.log("res", res);
 
-          if (res && res.length > 0) {
-            // user exists
-          } else {
-            // create user
-            await fetch(`${BASE_URL}/api/users/createUser`, {
-              method: "POST",
-              body: JSON.stringify({
-                name: user.name,
-                email: user.email,
-              }),
-              headers: {
-                "Content-type": "application/json; charset=UTF-8",
-              },
-            });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      //     if (res && res.length > 0) {
+      //       // user exists
+      //     } else {
+      //       // create user
+      //       await fetch(`${BASE_URL}/api/users/createUser`, {
+      //         method: "POST",
+      //         body: JSON.stringify({
+      //           name: user.name,
+      //           email: user.email,
+      //         }),
+      //         headers: {
+      //           "Content-type": "application/json; charset=UTF-8",
+      //         },
+      //       });
+      //     }
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
       return true;
     },
     async session({ session }) {
